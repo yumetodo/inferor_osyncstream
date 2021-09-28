fe89445b8740364044b14ddfc47a0d93f612d806
yumetodo <yume-wikijp@live.jp>
2021-09-29 00:53:29 +0900

    feat: impl move ctor/assign op and swap

--- include/inferior/streambuf_locks.hpp
+++ include/inferior/streambuf_locks.hpp
@@ -13,20 +13,40 @@ public:
         std::shared_ptr<std::mutex>&& lock, void* streambuf, streambuf_locks* locks) noexcept
         : lock(std::move(lock)), streambuf(streambuf), locks(locks) {}
     streambuf_lock_proxy(const streambuf_lock_proxy&) = delete;
-    streambuf_lock_proxy(streambuf_lock_proxy&&) = default;
+    streambuf_lock_proxy(streambuf_lock_proxy&& o) noexcept
+        : lock(o.lock), streambuf(o.streambuf), locks(locks) {
+        o.streambuf = nullptr;
+        o.locks = nullptr;
+    }
     streambuf_lock_proxy& operator=(const streambuf_lock_proxy&) = delete;
-    streambuf_lock_proxy& operator=(streambuf_lock_proxy&&) = default;
-    ~streambuf_lock_proxy() noexcept;
+    streambuf_lock_proxy& operator=(streambuf_lock_proxy&& o) noexcept {
+        this->dispose();
+        this->lock = std::move(o.lock);
+        this->streambuf = o.streambuf;
+        this->locks = o.locks;
+        o.streambuf = nullptr;
+        o.locks = nullptr;
+    }
+    ~streambuf_lock_proxy() noexcept { this->dispose(); }
+    void swap(streambuf_lock_proxy& o) noexcept {
+        using std::swap;
+        swap(this->lock, o.lock);
+        swap(this->streambuf, o.streambuf);
+        swap(this->locks, o.locks);
+    }
 
     bool operator!() const noexcept { return !this->lock; }
     explicit operator bool() const noexcept { return !(*this); }
     operator std::mutex& () const noexcept { return *lock; }
 
 private:
+    void dispose() noexcept;
     std::shared_ptr<std::mutex> lock;
     void* streambuf;
     streambuf_locks* locks;
 };
+void swap(streambuf_lock_proxy& l, streambuf_lock_proxy& r) noexcept { l.swap(r); }
+
 class streambuf_locks {
 private:
     streambuf_locks() = default;
@@ -68,9 +88,11 @@ private:
     std::unordered_map<void*, std::weak_ptr<std::mutex>> thelocks{};
     using guard = std::lock_guard<std::mutex>;
 };
-inline streambuf_lock_proxy::~streambuf_lock_proxy() noexcept {
-    if (locks) locks->release_lock(this->lock, this->streambuf);
+inline void streambuf_lock_proxy::dispose() noexcept {
+    try {
+        if (locks) locks->release_lock(this->lock, this->streambuf);
+    } catch (...) {
+    }
 }
-
 }  // namespace inferior::detail
 #endif  // INFERIOR_STREAMBUF_LOCKS_HPP_
