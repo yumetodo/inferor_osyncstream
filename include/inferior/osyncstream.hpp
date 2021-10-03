625a516d2d4ccd09c69a880664418c686e473d7e
yumetodo <yume-wikijp@live.jp>
2021-10-03 09:24:14 +0000

    BSL-1.0

--- include/inferior/osyncstream.hpp
+++ include/inferior/osyncstream.hpp
@@ -1,3 +1,8 @@
+/*=============================================================================
+  Copyright (C) 2021 yumetodo <yume-wikijp@live.jp>
+  Distributed under the Boost Software License, Version 1.0.
+  (See https://www.boost.org/LICENSE_1_0.txt)
+=============================================================================*/
 #ifndef INFERIOR_OSTNCSTREAM_HPP_
 #define INFERIOR_OSTNCSTREAM_HPP_
 #include <iosfwd>
